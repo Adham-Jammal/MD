af40454d85962651ac03569e21fe8883b6ecfca0
Adham <Adham-461@hotmail.com>
2022-08-23 03:56:13 +0300

    last updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,14 +35,19 @@
         <div class="container">
             <h2 class="about-title" data-aos="zoom-in" data-aos-duration="1000">{{ __('index.8') }} </h2>
             <div class="row">
-                <div class="column col-md-5">
+                <div class="column col-md-6">
                     <div class="content" data-aos="zoom-in" data-aos-duration="500">
                         <h4>{{ __('index.9') }} </h4>
                         <p>{{ __('index.10') }} </p>
                     </div>
-
                 </div>
-                <div class="column col-6 col-md-3">
+                <div class="column col-md-6">
+                    <div class="content" data-aos="zoom-in" data-aos-duration="500">
+                        <h4>{{ __('index.22') }} </h4>
+                        <p>{{ __('index.23') }} </p>
+                    </div>
+                </div>
+                <div class="column col-6 col-md-6">
                     <div class="content" data-aos="zoom-in" data-aos-duration="1000">
                         <h4>{{ __('index.11') }} </h4>
                         <p>{{ __('index.12') }} </p>
@@ -50,7 +55,7 @@
                 </div>
 
 
-                <div class="column col-6 col-md-4  ">
+                <div class="column col-6 col-md-6">
                     <div class="content" data-aos="zoom-in" data-aos-duration="1500">
                         <h4> {{ __('index.13') }} </h4>
                         <ul>
@@ -95,6 +100,64 @@
             </div>
         </div>
     </section>
+    <section class="cards-section">
+        <div class="container">
+            <div class="cards">
+                {{-- <div class="card">
+                    <h2>{{ __('index.22') }}</h2>
+                    <hr>
+                    <p>{{ __('index.23') }} </p>
+                </div> --}}
+
+                <div class="card">
+                    <h2>{{ __('index.24') }}</h2>
+                    <hr>
+                    <p>{{ __('index.25') }} </p>
+                </div>
+
+                <div class="card">
+                    <h2>{{ __('index.26') }}</h2>
+                    <hr>
+                    <p>{{ __('index.27') }} </p>
+                </div>
+
+                <div class="card">
+                    <h2>{{ __('index.28') }}</h2>
+                    <hr>
+                    <p>{{ __('index.29') }} </p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    {{-- advantages --}}
+    <section id="advantages" class="advantages">
+        <div class="container">
+            <h2 data-aos="zoom-in" data-aos-duration="1000">{{ __('index.20') }} </h2>
+            <div class="row">
+                <div class="col-md-6 image-content" data-aos="fade-right" data-aos-duration="1000">
+                    <img src="{{ asset('img/advantages.png') }}" alt="advantages-image">
+                </div>
+                <div class="col-md-6 text-content" data-aos="fade-left" data-aos-duration="1000">
+                    <p>{{ __('index.21') }}
+                    </p>
+                    {{-- <ul>
+                        <li>Determine requirements and work plan </li>
+                        <li>Development and implementation </li>
+                        <li> Visualization and exchange of ideas </li>
+                        <li>Determine requirements and work plan</li>
+                    </ul> --}}
+                    {{-- <a href="#">VIEW MORE<span class="arrow"><svg class="circle-svg" viewBox="0 0 100 100">
+                                <path
+                                    d="M72.3 27.41 C 74.53166666666667 29.97 78.22 33.70666666666666 79.59 36.82 80.96000000000001 39.93333333333334 80.10833333333333 42.79666666666667 80.52 46.09 80.93166666666666 49.38333333333334 82.13666666666667 53.36 82.06 56.58 81.98333333333333 59.8 81.79 62.346666666666664 80.06 65.41 78.33 68.47333333333333 74.51833333333335 72.82333333333332 71.68 74.96 68.84166666666667 77.09666666666666 65.95 76.95666666666668 63.03 78.23 60.11 79.50333333333333 57.364999999999995 81.62166666666667 54.16 82.6 50.955 83.57833333333332 46.968333333333334 84.47333333333333 43.8 84.1 40.63166666666666 83.72666666666666 37.946666666666665 82.13666666666667 35.15 80.36 32.35333333333333 78.58333333333333 29.301666666666666 76.33666666666666 27.02 73.44 24.738333333333333 70.54333333333334 22.775000000000002 66.185 21.46 62.98 20.145 59.775 19.988333333333333 57.33 19.13 54.21 18.271666666666665 51.09 16.044999999999998 47.78333333333333 16.31 44.26 16.575 40.736666666666665 18.796666666666667 36.21333333333333 20.72 33.07 22.64333333333333 29.926666666666666 25.378333333333334 27.341666666666665 27.85 25.4 30.32166666666667 23.458333333333332 32.541666666666664 22.683333333333334 35.55 21.42 38.55833333333333 20.15666666666667 42.42 18.196666666666665 45.9 17.82 49.379999999999995 17.443333333333335 53.04666666666667 18.553333333333335 56.43 19.16 59.81333333333333 19.766666666666666 63.55500000000001 20.085 66.2 21.46 68.845 22.835 70.06833333333333 24.85 72.3 27.41z">
+                                </path>
+                            </svg>&#10230;</span></a> --}}
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section id="works" class="works" data-aos="fade-up" data-aos-duration="1000">
         <div class="container">
             <div class="row">
@@ -125,33 +188,9 @@
 
         </div>
     </section>
-    {{-- advantages --}}
-    <section id="advantages" class="advantages">
-        <div class="container">
-            <h2 data-aos="zoom-in" data-aos-duration="1000">{{ __('index.20') }} </h2>
-            <div class="row">
-                <div class="col-md-6 image-content" data-aos="fade-right" data-aos-duration="1000">
-                    <img src="{{ asset('img/advantages.png') }}" alt="advantages-image">
-                </div>
-                <div class="col-md-6 text-content" data-aos="fade-left" data-aos-duration="1000">
-                    <p>{{ __('index.21') }}
-                    </p>
-                    {{-- <ul>
-                        <li>Determine requirements and work plan </li>
-                        <li>Development and implementation </li>
-                        <li> Visualization and exchange of ideas </li>
-                        <li>Determine requirements and work plan</li>
-                    </ul> --}}
-                    {{-- <a href="#">VIEW MORE<span class="arrow"><svg class="circle-svg" viewBox="0 0 100 100">
-                                <path
-                                    d="M72.3 27.41 C 74.53166666666667 29.97 78.22 33.70666666666666 79.59 36.82 80.96000000000001 39.93333333333334 80.10833333333333 42.79666666666667 80.52 46.09 80.93166666666666 49.38333333333334 82.13666666666667 53.36 82.06 56.58 81.98333333333333 59.8 81.79 62.346666666666664 80.06 65.41 78.33 68.47333333333333 74.51833333333335 72.82333333333332 71.68 74.96 68.84166666666667 77.09666666666666 65.95 76.95666666666668 63.03 78.23 60.11 79.50333333333333 57.364999999999995 81.62166666666667 54.16 82.6 50.955 83.57833333333332 46.968333333333334 84.47333333333333 43.8 84.1 40.63166666666666 83.72666666666666 37.946666666666665 82.13666666666667 35.15 80.36 32.35333333333333 78.58333333333333 29.301666666666666 76.33666666666666 27.02 73.44 24.738333333333333 70.54333333333334 22.775000000000002 66.185 21.46 62.98 20.145 59.775 19.988333333333333 57.33 19.13 54.21 18.271666666666665 51.09 16.044999999999998 47.78333333333333 16.31 44.26 16.575 40.736666666666665 18.796666666666667 36.21333333333333 20.72 33.07 22.64333333333333 29.926666666666666 25.378333333333334 27.341666666666665 27.85 25.4 30.32166666666667 23.458333333333332 32.541666666666664 22.683333333333334 35.55 21.42 38.55833333333333 20.15666666666667 42.42 18.196666666666665 45.9 17.82 49.379999999999995 17.443333333333335 53.04666666666667 18.553333333333335 56.43 19.16 59.81333333333333 19.766666666666666 63.55500000000001 20.085 66.2 21.46 68.845 22.835 70.06833333333333 24.85 72.3 27.41z">
-                                </path>
-                            </svg>&#10230;</span></a> --}}
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="partners" class="mission" data-aos="zoom-in" data-aos-duration="1000">
+    
+    {{-- Old Slider Section --}}
+    {{-- <section id="partners" class="mission" data-aos="zoom-in" data-aos-duration="1000">
         <div class="container">
             <div class="slider owl-carousel owl-drag owl-theme">
                 <div class="slide">
@@ -173,24 +212,30 @@
             </div>
         </div>
         <span>”</span>
-    </section>
+    </section> --}}
+
     <section class="gallery">
         <div class="container">
             <div class="images" data-aos="fade-up" data-aos-duration="1000">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
-                <img src="{{ asset('img/gallery-image-1.jpg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-1.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-2.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-3.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-4.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-5.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-6.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-7.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-8.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-9.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-10.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-11.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-12.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-13.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-14.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-15.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-16.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-17.jpeg') }}" alt="galeery-image">
+                <img src="{{ asset('img/gallery-image-18.jpeg') }}" alt="galeery-image">
+
             </div>
 
         </div>
